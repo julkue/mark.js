094dcd08afe1140318851a8eafcb287aaaa14222
Julian Motz <me@julianmotz.com>
2015-01-23 11:10:24 +0100

    Added support for requireJS and mapping functions

--- src/jquery.jmHighlight.js
+++ src/jquery.jmHighlight.js
@@ -1,5 +1,5 @@
 /**
-jmHighlight
+jmHighlight v1.0
 
 Fork of
 highlight v5
@@ -17,52 +17,72 @@ Johann Burkard
 <http://johannburkard.de>
 <mailto:jb@eaio.com>
 */
-
-jQuery.fn.highlight = function(pat, cssClass_) {
-	function innerHighlight(node, pat) {
-		var skip = 0;
-		if (node.nodeType == 3) {
-			var pos = node.data.toUpperCase().indexOf(pat);
-			pos -= (node.data.substr(0, pos).toUpperCase().length - node.data
-					.substr(0, pos).length);
-			if (pos >= 0) {
-				var spannode = document.createElement('span');
-				if(typeof cssClass_ === "string"){
-					spannode.className = cssClass_;
-				} else {
-					spannode.className = 'highlight';
-				}
-				var middlebit = node.splitText(pos);
-				var endbit = middlebit.splitText(pat.length);
-				var middleclone = middlebit.cloneNode(true);
-				spannode.appendChild(middleclone);
-				middlebit.parentNode.replaceChild(spannode, middlebit);
-				skip = 1;
-			}
-		} else if (node.nodeType == 1 && node.childNodes
-					&& !/(script|style)/i.test(node.tagName)) {
-			for (var i = 0; i < node.childNodes.length; ++i) {
-				i += innerHighlight(node.childNodes[i], pat);
-			}
-		}
-		return skip;
-	}
-	return this.length && pat && pat.length ? this.each(function() {
-		innerHighlight(this, pat.toUpperCase());
-	}) : this;
-};
-
-jQuery.fn.removeHighlight = function(cssClass_) {
-	if(typeof cssClass_ === "string"){
-		var c = cssClass_;
+(function (global, factory) {
+	'use strict';
+	if(typeof define === 'function' && define.amd) {
+		// RequireJS. Register as an anonymous module.
+		define(['jquery'], function(jQuery) {
+			return factory(jQuery, global);
+		});
+	} else if (typeof exports === 'object') {
+		 // Node/CommonJS
+		 factory(require('jquery'), global);
 	} else {
-		var c = "highlight";
+		// Browser globals
+		factory(global.jQuery, global);
 	}
-	return this.find("span." + c).each(function() {
-		this.parentNode.firstChild.nodeName;
-		with(this.parentNode) {
-			replaceChild(this.firstChild, this);
-			normalize();
+})(this, function (jQuery, global_) {
+	jQuery.fn.highlight = function(pat, cssClass_) {
+		function innerHighlight(node, pat) {
+			var skip = 0;
+			if (node.nodeType == 3) {
+				var pos = node.data.toUpperCase().indexOf(pat);
+				pos -= (node.data.substr(0, pos).toUpperCase().length - node.data
+						.substr(0, pos).length);
+				if (pos >= 0) {
+					var spannode = document.createElement('span');
+					if(typeof cssClass_ === "string"){
+						spannode.className = cssClass_;
+					} else {
+						spannode.className = 'highlight';
+					}
+					var middlebit = node.splitText(pos);
+					var endbit = middlebit.splitText(pat.length);
+					var middleclone = middlebit.cloneNode(true);
+					spannode.appendChild(middleclone);
+					middlebit.parentNode.replaceChild(spannode, middlebit);
+					skip = 1;
+				}
+			} else if (node.nodeType == 1 && node.childNodes
+						&& !/(script|style)/i.test(node.tagName)) {
+				for (var i = 0; i < node.childNodes.length; ++i) {
+					i += innerHighlight(node.childNodes[i], pat);
+				}
+			}
+			return skip;
 		}
-	}).end();
-};
+		return this.length && pat && pat.length ? this.each(function() {
+			innerHighlight(this, pat.toUpperCase());
+		}) : this;
+	};
+	jQuery.fn.jmHighlight = function(){
+		jQuery.fn.highlight.apply(this, arguments);
+	};
+	jQuery.fn.removeHighlight = function(cssClass_) {
+		if(typeof cssClass_ === "string"){
+			var c = cssClass_;
+		} else {
+			var c = "highlight";
+		}
+		return this.find("span." + c).each(function() {
+			this.parentNode.firstChild.nodeName;
+			with(this.parentNode) {
+				replaceChild(this.firstChild, this);
+				normalize();
+			}
+		}).end();
+	};
+	jQuery.fn.jmRemoveHighlight = function(){
+		jQuery.fn.removeHighlight.apply(this, arguments);
+	};
+});
