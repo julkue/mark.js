a73273b86702c5919f605e66d404feb528c0c000
Julian Motz <me@julianmotz.com>
2015-02-12 12:25:58 +0100

    Added new API and support for filter elements and wrapper-options. Removed fork from Johann Burkard and created plugin since it was not builded for filter- and class-options.

--- README.md
+++ README.md
@@ -1,35 +1,58 @@
 jmHighlight
 ==============
 
-###### JavaScript: Highlight. The plugin will highlight keywords in a defined element recursive with a defined class.
+###### JavaScript: Highlight keywords/phrases in a context with a defined class. Can be used e.g. to highlight search results on page.
 
 Usage
 --------
+To highlight a keyword/phrase you can define a element that will be wrapped around the
+keyword. The default element is `span`. Also you can define a class that will be appended
+to the wrapper. The default class is `highlight`. 
+
+If you want to ignore some elements in the context, e.g. a specific element with a class you need to
+pass a object "filter" in the option-object. The filter-object should contain an array
+of all selectors, that should be ignored.
+
+You can also highlight multiple keywords/phrases in the context. Just clone this code below.
 ```javascript
-// With default class name "highlight"
-$("yourSelector").jmHighlight("yourKeyword");
-// With custom class name
-$("yourSelector").jmHighlight("yourKeyword", "yourClass");
-// To remove highlighting
-$("yourSelector").jmRemoveHighlight("yourOptionalClass");
+// Will highlight each keyword "lorem" in the context ".test"
+// It will ignore all keywords inside ".noHighlight" and ".ignore".
+// The wrapper element will be a "em"-element with the class "customHighlight"
+$(".test").jmHighlight("lorem", {
+     // Optional
+    "filter": [
+        ".noHighlight",
+        ".ignore"
+    ],
+    // Optional. Default is "span"
+    "element": "em",
+    // Optional. Default is "highlight"
+    "className": "customHighlight"
+});
 ```
-If you want to ignore some elements then filter the elements yourself like
+You can remove the highlight in a specific context by
+ * a specific class (in our example above "customHighlight")
+ * a specific element
+ * or a keyword
+ 
+You can combine them as the code below shows. If you don't
+pass any constraint all highlights will be removed.
+
 ```javascript
-$("yourSelector").filter(function(){
-	// Make your filter (return false if the
-	// element should be removed from stack
-	// otherwise true)
-}).jmHighlight("yourKeyword");
+$(".test").jmRemoveHighlight({
+    "filter": [
+        ".noHighlight",
+        ".ignore"
+    ],
+    "element": "span",
+    "className": "customHighlight"
+}, "lorem");
+		
 ```
 
 Demo
 --------
 See /test folder.
 
-
-
-
-This plugin is a fork from Johann Burkards highlight [Plugin](http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html).
-It adds the feature to use custom classes and brings support for AMD-modules like RequireJS, but also support for Node/CommonJS.
 
 _License: The MIT License (MIT)_
--- src/jquery.jmHighlight.js
+++ src/jquery.jmHighlight.js
@@ -1,22 +1,9 @@
-/**
-jmHighlight v1.0
-
-Fork of
-highlight v5
-
-Highlights arbitrary terms.
-
-<http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html>
-
-MIT license.
-
-Modified by julmot to allow custom css classes.
-<https://github.com/julmot>
-
-Johann Burkard
-<http://johannburkard.de>
-<mailto:jb@eaio.com>
-*/
+/*!***************************************************
+ * jmHighlight
+ * Version 2.0.0
+ * Copyright (c) 2015, Julian Motz
+ * Released under the MIT license
+ *****************************************************/
 (function (global, factory) {
 	'use strict';
 	if(typeof define === 'function' && define.amd) {
@@ -32,57 +19,314 @@ Johann Burkard
 		factory(global.jQuery, global);
 	}
 })(this, function (jQuery, global_) {
-	jQuery.fn.highlight = function(pat, cssClass_) {
-		function innerHighlight(node, pat) {
-			var skip = 0;
-			if (node.nodeType == 3) {
-				var pos = node.data.toUpperCase().indexOf(pat);
-				pos -= (node.data.substr(0, pos).toUpperCase().length - node.data
-						.substr(0, pos).length);
-				if (pos >= 0) {
-					var spannode = document.createElement('span');
-					if(typeof cssClass_ === "string"){
-						spannode.className = cssClass_;
-					} else {
-						spannode.className = 'highlight';
+	"use strict";
+	
+	/**
+	 * Map jQuery
+	 */
+	var $ = jQuery;
+	
+	/**
+	 * For debug purposes
+	 */
+	var _debug = false;
+	
+	/**
+	 * Default options
+	 */
+	var defaults = {
+		"element": "span",
+		"className": "highlight"
+	};
+	
+	/**
+	 * Inits the highlight component
+	 * 
+	 * @param string keyword_
+	 * @param jquery-object $context_
+	 * @param object options_
+	 * @return bool
+	 */
+	function initHighlight(keyword_, $context_, options_){
+		
+		if($context_ instanceof $ == false || typeof keyword_ !== "string"
+			|| keyword_ == ""){
+			return false;
+		}
+		
+		// Get all nodes inside the context, but do not search in nodes
+		// that were already highlighted
+		var $contextElements = $context_.find("*:not([data-jmHighlight])");
+		
+		// Filter elements
+		if(typeof options_ === "object" && typeof options_["filter"] === "object"){
+			var tmp = filter($contextElements, options_["filter"]);
+			if(tmp != false){
+				$contextElements = tmp;
+			}
+		}
+		
+		// Highlight elements
+		highlight(keyword_, $contextElements, options_);
+		
+		return true;
+		
+	}
+	
+	/**
+	 * Inits the remove highlight component
+	 * 
+	 * @param jquery-object $context_
+	 * @param string keyword_ (optional)
+	 */
+	function initRemoveHighlight($context_, options_, keyword_){
+		
+		if(typeof $context_ === "undefined" || $context_ instanceof $ == false){
+			return false;
+		}
+		
+		// Get all nodes inside the context
+		var $contextElements = $context_.find("*:not([data-jmHighlight])");
+		
+		// Filter elements
+		if(typeof options_ === "object" && typeof options_["filter"] === "object"){
+			var tmp = filter($contextElements, options_["filter"]);
+			if(tmp != false){
+				$contextElements = tmp;
+			}
+		}
+		
+		// Remove highlight
+		removeHighlight($contextElements, options_, keyword_);
+		
+		return true;
+		
+	}
+	
+	/**
+	 * Highlights a keyword in a stack of elements
+	 * 
+	 * @param string keyword_
+	 * @param jquery-object $elements_
+	 * @param object options
+	 * 			* element
+	 * 			* className
+	 * @return bool
+	 */
+	function highlight(keyword_, $elements_, options_){
+		
+		if($elements_ instanceof $ == false || $elements_.length == 0
+			|| typeof keyword_ !== "string" || keyword_ == ""){
+			return false;
+		}
+		
+		if(_debug){
+			console.log("Highlighting keyword '" + keyword_ + "' in elements:");
+			console.log($elements_);
+		}
+		
+		if(typeof options_ !== "object"){
+			options_ = {};
+		}
+		if(typeof options_["element"] !== "string"){
+			options_["element"] = defaults["element"];
+		}
+		if(typeof options_["className"] !== "string"){
+			options_["className"] = defaults["highlight"];
+		}
+		
+		// Iterate over all text nodes and replace
+		// the search keyword after finding (case insensitive)
+		forEachTextNodes($elements_, function(node_){
+			
+			var node = node_;
+			if(node.nodeValue.toLowerCase().indexOf(keyword_.toLowerCase()) == -1){
+				return true;
+			}
+			var tagO = "<" + options_["element"] + " class='" + options_["className"] +
+						"' data-jmHighlight='true'>";
+			var tagC = "</" + options_["element"] + ">";
+			if(node.parentNode != null){
+				var regex = new RegExp(keyword_, "gi");
+				// Save the original keyword before replacing since
+				// the original keyword may contain some letters upper/lower-case.
+				// The keyword maybe not.
+				var originalKeyword = regex.exec(node.parentNode.innerHTML)[0];
+				node.parentNode.innerHTML = node.parentNode.innerHTML.replace(
+					regex,
+					tagO + originalKeyword + tagC
+				);
+			}
+			
+		});
+		
+		return true;
+		
+	}
+	
+	/**
+	 * Removes the highlight in a stack of elements. The keyword
+	 * is optional. If none is defined, every keywords will be removed.
+	 * 
+	 * @param jquery-object $elements_
+	 * @param object options
+	 * 			* element
+	 * 			* className
+	 * @param string keyword_
+	 * @return bool
+	 */
+	function removeHighlight($elements_, options_, keyword_){
+		
+		if($elements_ instanceof $ == false || $elements_.length == 0){
+			return false;
+		}
+		
+		if(_debug){
+			console.log("Remove highlight");
+			if(typeof keyword_ === "string" && keyword_ != ""){
+				console.log("with keyword: '" + keyword_ + "'");
+			}
+		}
+		
+		if(typeof options_ !== "object"){
+			options_ = {};
+		}
+		if(typeof options_["element"] !== "string"){
+			options_["element"] = defaults["element"];
+		}
+		if(typeof options_["className"] !== "string"){
+			options_["className"] = defaults["highlight"];
+		}
+		
+		// Iterate over all text nodes
+		$elements_.each(function(){
+			
+			var $this = $(this);
+			var $highlightElements = $this.find(
+				options_["element"] + "." + options_["className"]
+			);
+			$highlightElements.each(function(){
+				
+				var $highlightEl = $(this);
+				if(typeof keyword_ === "string" && keyword_ != "" &&
+					$highlightEl.text().toLowerCase().indexOf(keyword_.toLowerCase()) == -1){
+					return;
+				} else {
+					// Remove element with his text
+					$highlightEl.replaceWith($highlightEl.text());
+				}
+				
+			});
+			
+		});
+		
+		return true;
+		
+	}
+	
+	/**
+	 * Filters elements defined in a array
+	 * 
+	 * @param jquery-object $elements_
+	 * @param array filter_
+	 * @return jquery-object or false
+	 */
+	function filter($elements_, filter_){
+		
+		// Filter elements if defined
+		if(typeof filter_ !== "object" || $elements_ instanceof $ == false){
+			return false;
+		}
+		var $contextElements = $elements_;
+		if(Object.prototype.toString.call(filter_) === '[object Array]'){
+			$contextElements = $contextElements.filter(function(){
+				
+				var $this = $(this);
+				var filterArr = filter_;
+				
+				// Check if match in element itself
+				var foundInElement = false;
+				filterArr.forEach(function(filter){
+					// We use is() instead of hasClass() to
+					// support complex selectors
+					if($this.is(filter)){
+						foundInElement = true;
+						return;
 					}
-					var middlebit = node.splitText(pos);
-					var endbit = middlebit.splitText(pat.length);
-					var middleclone = middlebit.cloneNode(true);
-					spannode.appendChild(middleclone);
-					middlebit.parentNode.replaceChild(spannode, middlebit);
-					skip = 1;
+				});
+				if(foundInElement){
+					// Delete entry
+					return false;
+				} else {
+					// Remain entry
+					return true;
 				}
-			} else if (node.nodeType == 1 && node.childNodes
-						&& !/(script|style)/i.test(node.tagName)) {
-				for (var i = 0; i < node.childNodes.length; ++i) {
-					i += innerHighlight(node.childNodes[i], pat);
+			});
+		}
+		return $contextElements;
+		
+	}
+	
+	/**
+	 * Gets non recursive nodes of an element and calls
+	 * the callback function on each text node
+	 * 
+	 * @param jquery-object $elements_
+	 * @return boolean
+	 */
+	function forEachTextNodes($elements_, callbackFn_){
+		
+		if(typeof $elements_ === "undefined" || $elements_ instanceof $ == false){
+			return false;
+		}
+		if(typeof callbackFn_ !== "function"){
+			return false;
+		}
+		
+		// Iterate over all items in the stack
+		var $tmp = $elements_.each(function(){
+			
+			var $this = $(this);
+			
+			// Get all text nodes of this element (not recursive!)
+			var $nodes = $this.contents().filter(function(){
+				if(this.nodeType == 3){
+					return true;
+				} else {
+					return false;
 				}
-			}
-			return skip;
-		}
-		return this.length && pat && pat.length ? this.each(function() {
-			innerHighlight(this, pat.toUpperCase());
-		}) : this;
+			});
+			
+			// Iterate over that text nodes and call callback
+			$nodes.each(function(){
+				
+				callbackFn_(this);
+				
+			});
+			
+		});
+		return true;
+		
+	}
+		
+	/**
+	 * Highlight component for jQuery
+	 * 
+	 * @return boolean
+	 */
+	jQuery.fn.jmHighlight = function(keyword_, options_){
+		
+		return initHighlight(keyword_, $(this), options_);
+		
 	};
-	jQuery.fn.jmHighlight = function(){
-		jQuery.fn.highlight.apply(this, arguments);
+	jQuery.fn.jmRemoveHighlight = function(options_, keyword_){
+		
+		return initRemoveHighlight($(this), options_, keyword_);
+		
 	};
-	jQuery.fn.removeHighlight = function(cssClass_) {
-		if(typeof cssClass_ === "string"){
-			var c = cssClass_;
-		} else {
-			var c = "highlight";
-		}
-		return this.find("span." + c).each(function() {
-			this.parentNode.firstChild.nodeName;
-			with(this.parentNode) {
-				replaceChild(this.firstChild, this);
-				normalize();
-			}
-		}).end();
-	};
-	jQuery.fn.jmRemoveHighlight = function(){
-		jQuery.fn.removeHighlight.apply(this, arguments);
-	};
+	
+	
 });
+
+
+
