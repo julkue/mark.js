1adc2c4f177aa3330ab96b98baba2b10adc6ea28
Julian Motz <me@julianmotz.com>
2016-07-14 18:43:48 +0200

    Added unit tests to check nested duplicated contexts

--- test/specs/basic-duplicate-context.js
+++ test/specs/basic-duplicate-context.js
@@ -5,27 +5,47 @@
  * Released under the MIT license https://git.io/vwTVl
  *****************************************************/
 "use strict";
-describe("basic mark with duplicate context elements", function () {
-    var $ctx1, called;
+describe("basic mark with duplicated contexts", function () {
+    var $ctx1, $ctx2, ctx1Called, ctx2Called;
     beforeEach(function (done) {
         loadFixtures("basic-duplicate-context.html");
 
-        $ctx1 = $(".basic-duplicate-context");
-        called = 0;
+        $ctx1 = $(".basic-duplicate-context > div:first-child");
+        $ctx2 = $(".basic-duplicate-context > div:last-child");
+        ctx1Called = ctx2Called = 0;
         new Mark([$ctx1[0], $ctx1[0]]).mark("test", {
             "diacritics": false,
             "separateWordSearch": false,
             "filter": function(){
-                // it should be called only once, as there's only one text node
-                called++;
+                ctx1Called++;
+                // return false. Otherwise matches would become wrapped and no
+                // further matches would be found. Therefore no further filter
+                // calls would be done
+                return false;
             },
             "done": function () {
-                done();
+                new Mark([$ctx2[0], $ctx2.find("span")[0]]).mark("test", {
+                    "filter": function(){
+                        ctx2Called++;
+                        // return false. Otherwise matches would become wrapped
+                        // and no further matches would be found. Therefore no
+                        // further filter calls would be done
+                        return false;
+                    },
+                    "done": function(){
+                        done();
+                    }
+                });
             }
         });
     });
 
-    it("should ignore duplicate context elements", function () {
-        expect(called).toBe(1);
+    it("should ignore duplicated passed contexts", function () {
+        // it should be called only once, as there's only one text node
+        expect(ctx1Called).toBe(1);
+    });
+    it("should ignore contexts inside other contexts", function(){
+        // it should be called only twice, as there are two text nodes
+        expect(ctx2Called).toBe(2);
     });
 });
