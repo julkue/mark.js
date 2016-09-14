2e576918ee3779a45a8f7e510317a1e9371e2aa5
Julian Motz <me@julianmotz.com>
2016-09-14 14:47:29 +0200

    Cleanup

--- test/specs/regexp/infinite.js
+++ test/specs/regexp/infinite.js
@@ -6,42 +6,39 @@
  *****************************************************/
 "use strict";
 describe("mark with regular expression with infinite results", function () {
-    var $ctx1, $ctx2, errorThrown;
+    var $ctx1, $ctx2, errorThrown1, errorThrown2;
     beforeEach(function (done) {
         loadFixtures("regexp/infinite.html");
 
         $ctx1 = $(".regexp-infinite > div:first-child");
         $ctx2 = $(".regexp-infinite > div:last-child");
-        errorThrown = false;
-        new Mark($ctx1[0]).markRegExp(/(|)/gmi, {
-            "done": function () {
-                try {
-                    new Mark($ctx2[0]).markRegExp(/\b/gmi, {
-                        "done": function () {
-                            // timeout, otherwise "ret =" will not be executed
-                            setTimeout(function () {
-                                done();
-                            }, 50);
-                        }
-                    });
-                } catch(e) {
-                    errorThrown = true;
-                    done();
+        errorThrown1 = errorThrown2 = false;
+        try {
+            new Mark($ctx1[0]).markRegExp(/(|)/gmi, {
+                "done": function () {
+                    try {
+                        new Mark($ctx2[0]).markRegExp(/\b/gmi, {
+                            "done": done
+                        });
+                    } catch(e) {
+                        errorThrown2 = true;
+                        done();
+                    }
                 }
-            }
-        });
+            });
+        } catch(e) {
+            errorThrown1 = true;
+            done();
+        }
     });
 
     it(
         "should not mark regular expressions with infinite matches",
         function () {
+            expect(errorThrown1).toBe(false);
+            expect(errorThrown2).toBe(false);
             expect($ctx1.find("mark")).toHaveLength(0);
-        }
-    );
-    it("should not mark regular expression with infinite matches on breaks",
-        function () {
             expect($ctx2.find("mark")).toHaveLength(0);
-            expect(errorThrown).toBe(false);
         }
     );
 });
