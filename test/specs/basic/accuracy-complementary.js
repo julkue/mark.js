31f101743eb1c2862e1fbe12a4a33c8e48ecbb6a
Julian Motz <me@julianmotz.com>
2017-02-04 15:24:03 +0100

    Intensified accuracy complementary unit tests

--- test/specs/basic/accuracy-complementary.js
+++ test/specs/basic/accuracy-complementary.js
@@ -25,10 +25,12 @@ describe("basic mark with accuracy complementary", function () {
         });
     });
 
-    it("should wrap the right matches", function () {
-        expect($ctx1.find("mark")).toHaveLength(2);
-        expect($ctx1.find("mark:first-child").text()).toBe("testLoremtest");
-        expect($ctx1.find("mark:last-child").text()).toBe("ipsumx");
+    it("should wrap the correct matches", function () {
+        expect($ctx1.find("mark")).toHaveLength(4);
+        var textOpts = ["testLoremtest", "ipsumx", "ipsumx-test", "öipsumxö"];
+        $ctx1.find("mark").each(function () {
+            expect($.inArray($(this).text(), textOpts)).toBeGreaterThan(-1);
+        });
     });
     it("should work with separateWordSearch", function () {
         var textOpts = ["testLorem", "ipsumtest"];
