990e8a6fac42febaac3faf951c826f7cb4bacba1
Julian Motz <me@julianmotz.com>
2017-02-05 13:13:28 +0100

    Update patch level

--- dist/jquery.mark.min.js
+++ dist/jquery.mark.min.js
@@ -1,5 +1,5 @@
 /*!***************************************************
- * mark.js v8.8.1
+ * mark.js v8.8.2
  * https://github.com/julmot/mark.js
  * Copyright (c) 2014–2017, Julian Motz
  * Released under the MIT license https://git.io/vwTVl
