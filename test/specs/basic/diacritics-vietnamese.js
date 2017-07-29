bb1e0296b278d6d420dce8479f38198b7525f058
The Tran <tran.q.the@gmail.com>
2017-07-30 04:36:06 +0700

    Support Vietnamese diacritics (#148)
    Closes #147

--- test/specs/basic/diacritics-vietnamese.js
+++ test/specs/basic/diacritics-vietnamese.js
@@ -5,23 +5,21 @@
  * Released under the MIT license https://git.io/vwTVl
  *****************************************************/
 "use strict";
-describe("basic mark with diacritics", function () {
+describe("basic mark with diacritics for Vietnamese", function () {
     var $ctx;
     beforeEach(function (done) {
-        loadFixtures("basic/diacritics.html");
+        loadFixtures("basic/diacritics-vietnamese.html");
 
-        $ctx = $(".basic-diacritics");
+        $ctx = $(".basic-diacritics-vietnamese");
         // including a term with a "s" and a whitespace to check "merge blanks"
         // behavior in combination with diacritics
-        new Mark($ctx[0]).mark(["dolor", "amet", "justo", "lores ipsum"], {
+        new Mark($ctx[0]).mark(["truong", "am", "ac"], {
             "separateWordSearch": false,
-            "done": function () {
-                done();
-            }
+            "done": done
         });
     });
 
     it("should treat normal and diacritic characters equally", function () {
-        expect($ctx.find("mark")).toHaveLength(15);
+        expect($ctx.find("mark")).toHaveLength(9);
     });
 });
