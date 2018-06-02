981cb991c54f969a401795275b0df14ddb7a3af2
Danny Lin <danny0838@gmail.com>
2018-06-02 22:43:40 +0800

    Fix regexp not escaped for a synonym set if wildcards is enabled. (#242)

--- test/specs/basic/wildcards-synonyms.js
+++ test/specs/basic/wildcards-synonyms.js
@@ -1,11 +1,12 @@
 'use strict';
 describe('basic mark with wildcards and synonyms', function() {
-  var $ctx1, $ctx2;
+  var $ctx1, $ctx2, $ctx3;
   beforeEach(function(done) {
     loadFixtures('basic/wildcards-synonyms.html');
 
     $ctx1 = $('.basic-wildcards-synonyms > div:nth-child(1)');
     $ctx2 = $('.basic-wildcards-synonyms > div:nth-child(2)');
+    $ctx3 = $('.basic-wildcards-synonyms > div:nth-child(3)');
     new Mark($ctx1[0]).mark('Lor?m', {
       'synonyms': {
         'Lor?m': 'Ips?m'
@@ -21,7 +22,18 @@ describe('basic mark with wildcards and synonyms', function() {
           'separateWordSearch': false,
           'diacritics': true,
           'wildcards': 'enabled',
-          'done': done
+          'done': function() {
+            new Mark($ctx3[0]).mark(['lorem', 'good(s)'], {
+              'synonyms': {
+                'lorem': '1+1',
+                'good(s)': 'ipsum'
+              },
+              'separateWordSearch': false,
+              'diacritics': false,
+              'wildcards': 'enabled',
+              'done': done
+            });
+          }
         });
       }
     });
@@ -30,5 +42,8 @@ describe('basic mark with wildcards and synonyms', function() {
   it('should match wildcards inside of synonyms', function() {
     expect($ctx1.find('mark')).toHaveLength(10);
     expect($ctx2.find('mark')).toHaveLength(17);
+  });
+  it('regexp special chars in each synonym set should be escaped', function() {
+    expect($ctx3.find('mark')).toHaveLength(4);
   });
 });
